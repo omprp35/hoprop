bd69bb0f5c20a4a6defd334734fa27d672e803e0
OM PRAPAN DEEP <faddyok2@gmail.com>
2026-08-20 13:30:24 +0530

    Delete src/check-extensions.js

--- src/check-extensions.js
+++
@@ -1,9 +0,0 @@
-const config = require('./config');
-const { extensionInfo } = require('./extensions');
-
-for (const info of [
-  extensionInfo(config.customExtensionDir, 'custom'),
-  extensionInfo(config.surfsharkExtensionDir, 'surfshark')
-]) {
-  console.log(JSON.stringify(info, null, 2));
-}
